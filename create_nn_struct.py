e43ea3aae359354d4061b77a28387db6febdbab6
AlexPasqua <alexpasquali98@gmail.com>
2020-08-07 12:34:01 +0200

    Improved create_nn_struct.py

--- create_nn_struct.py
+++ create_nn_struct.py
@@ -7,6 +7,68 @@ create the net's structure in Keras
 import argparse
 
 
+# Dictionary containing the base strings for each type of known layer
+layers_strings = {
+    'Input': "input = Input(shape=({},{},{}), name='{}')\n",
+    'Convolution': "{} = Conv2D(name='{}', filters={}, kernel_size={}, strides={}, padding='same')({})\n",
+    'ReLU': "{} = ReLU()({})\n",
+    'PReLU': "{} = PReLU(name='{}')({})\n",
+    'Concat': "{} = Concatenate(name='{}')([{}])\n\n",
+    'Pooling': "{} = {}Pooling2D(pool_size={}, strides={}, padding='valid')({})\n",
+    'unknown': "\nUNKNOWN Layer --> line: {}\tname: {}\t type: {}\n\n",
+}
+
+
+def write_layer(layer_data, outfile):
+    """
+    Writes one layer's data on the output file
+
+    Arguments:
+        layer_data: Dictionary containing one layer's data
+        outfile: the output file (in order to be able to write on it)
+
+    Returns: layer_data modified -> layer_data['bottoms'] is reset
+    """
+
+    known_types = ['Convolution', 'ReLU', 'Pooling', 'PReLU', 'Concat']
+    pool_type = {'MAX':'Max', 'AVG':'Average'}
+
+    if layer_data['type'] == 'Convolution':
+        outfile.write(layers_strings['Convolution'].format(layer_data['name'],
+                                                           layer_data['name'],
+                                                           layer_data['num_output'],
+                                                           layer_data['kernel_size'],
+                                                           layer_data['stride'],
+                                                           write_layer.prev_layer_top))
+
+    elif layer_data['type'] == 'Pooling':
+        outfile.write(layers_strings['Pooling'].format(layer_data['name'],
+                                                       pool_type[layer_data['pool']],
+                                                       layer_data['kernel_size'],
+                                                       layer_data['stride'],
+                                                       write_layer.prev_layer_top))
+
+    elif layer_data['type'] == 'ReLU':
+        outfile.write(layers_strings['ReLU'].format(write_layer.prev_layer_name, write_layer.prev_layer_name))
+
+    elif layer_data['type'] == 'PReLU':
+        outfile.write(layers_strings['PReLU'].format(write_layer.prev_layer_name, layer_data['name'], write_layer.prev_layer_name))
+
+    elif layer_data['type'] == 'Concat':
+        bottoms_str = ''
+        for bottom in layer_data['bottoms']:
+            bottoms_str = bottoms_str + bottom + ', '
+        outfile.write(layers_strings['Concat'].format(layer_data['name'], layer_data['name'], bottoms_str[ : -2]))
+
+    elif layer_data['type'] not in known_types:
+        outfile.write(layers_strings['unknown'].format(i, layer_data['name'], layer_data['type']))
+
+    layer_data['bottoms'] = []
+    write_layer.prev_layer_name = layer_data['name']
+    write_layer.prev_layer_top = layer_data['top']
+    return layer_data
+
+
 def write_nn_struct_code_keras(argv):
     """
     Reads the Caffe model's definition from its prototxt file and writes on the outfile
@@ -20,24 +82,18 @@ def write_nn_struct_code_keras(argv):
     Returns: None
     """
 
-    known_types = ['Convolution', 'ReLU', 'Pooling', 'PReLU', 'Concat']
-    # Dictionary containing the base strings for each type of known layer
-    layers_strings = {
-        'Input': "input = Input(shape=({},{},{}), name='{}')\n",
-        'Convolution': "{} = Conv2D(name='{}', filters={}, kernel_size={}, strides={}, padding='same')({})\n",
-        'ReLU': "{} = ReLU()({})\n",
-        'PReLU': "{} = PReLU(name='{}')({})\n",
-        'Concat': "{} = Concatenate(name='{}')([{}])\n\n",
-        'Pooling': "{} = {}Pooling2D(pool_size={}, strides={}, padding='valid')({})\n",
-        'unknown': "\nUNKNOWN Layer --> line: {}\tname: {}\t type: {}\n\n",
-    }
+
     # Dictionary containing one layer's data
     layer_data = {'name': '', 'type': '', 'bottoms': [], 'top': '',
-        'num_output': 0, 'pool_type': 'MAX', 'kernel_size': 0, 'stride': 1,
+        'num_output': 0, 'pool': 'MAX', 'kernel_size': 0, 'stride': 1
     }
-    pool_type = {'MAX':'Max', 'AVG':'Average'}
-    prev_layer_top = ''
-    prev_layer_name = ''
+
+    # These 2 variables are persistent in write_layer: even when the function ends, they keep their value
+    write_layer.prev_layer_top = ''
+    write_layer.prev_layer_name = ''
+
+    curls_count = 0     # counter of open (and not closed yet) curly brackets in the prototxt
+    inparam = False     # True if the current line is inside the convolution_param / pooling_param of a layer
 
     # Read the prototxt
     # Update layer_data
@@ -47,84 +103,57 @@ def write_nn_struct_code_keras(argv):
         with open(argv.outfile, 'w') as outfile:
             offset = 0
             while 'input:' not in lines[offset]:
+                if 'layer {' in lines[offset]: break    # in case there's no input
                 offset = offset + 1
 
             # Write the input layer
             if 'input:' in lines[offset]:
-                input_name = lines[offset][8 : -2]
-                shape = (lines[3 + offset][11], lines[4 + offset][11], lines[2 + offset][11])
-                prev_layer_top = 'input'
+                input_name = lines[offset].split()[1].strip('"')
+                map = {3:0, 4:1, 2:2}
+                shape = [0,0,0]
+                for i in map.keys():
+                    v = [int(s) for s in lines[i + offset].split() if s.isdigit()]
+                    shape[map[i]] = v[1] if len(v) > 1 else v[0]
+                write_layer.prev_layer_top = 'input'
                 outfile.write(layers_strings['Input'].format(shape[0], shape[1], shape[2], input_name))
 
             # Scan the rest of the prototxt
             for i in range(5 + offset, len(lines)):
                 curr = lines[i]
-                if 'name:' in curr:
-                    layer_data['name'] = curr[9 : -2]
+                if '{' in curr:     # It means that we're entering a section
+                    curls_count = curls_count + 1
+                elif '}' in curr:
+                    curls_count = curls_count - 1
+                    if curls_count == 1 and inparam: inparam = False
+                    elif curls_count == 0: layer_data = write_layer(layer_data, outfile)
                     continue
 
-                elif 'type:' in curr:
-                    layer_data['type'] = curr[9 : -2]
-                    if layer_data['type'] not in known_types:
-                        outfile.write(layers_strings['unknown'].format(i, layer_data['name'], layer_data['type']))
-                    continue
+                # If curr is a line which is not part of a subsection of a layer
+                if curls_count == 1:
+                    keywords = ('name:', 'type:', 'top:', 'bottom:')
+                    for kw in keywords:
+                        if kw in curr:
+                            datum = curr.split()[1].strip('"')
+                            if kw == 'bottom:': layer_data['bottoms'].append(datum)
+                            else: layer_data[kw[:-1]] = datum
 
-                elif 'bottom:' in curr:
-                    if layer_data['type'] == 'Concat':
-                        layer_data['bottoms'].append(curr[11 : -2])
-                    continue
+                # Otherwise it has to read the convolution_param / pooling_param
+                elif curls_count == 2:
+                    if 'convolution_param {' in curr or 'pooling_param {' in curr:
+                        inparam = True
+                        default_stride = True
+                        continue
 
-                elif 'top:' in curr:
-                    layer_data['top'] = curr[8 : -2]
-                    continue
+                    if inparam:
+                        keywords = ('num_output:', 'pool:', 'kernel_size:', 'stride:')
+                        for kw in keywords:
+                            if kw in curr:
+                                layer_data[kw[:-1]] = curr.split()[1]
+                                if kw == 'stride:': default_stride = False
 
-                elif 'convolution_param {' in curr or 'pooling_param {' in curr:
-                    #print(layer_data['type'], '\t\t', layer_data['name'])
-                    layer_data['stride'] = 1    # stride default value
-                    # read all the convolution / pooling parameters
-                    j = 1
-                    while '}' not in lines[i+j]:
-                        cursor = lines[i+j]
-                        if 'num_output:' in cursor:
-                            layer_data['num_output'] = cursor[-4 : -1].strip()
-                        elif 'pool:' in cursor:
-                            layer_data['pool_type'] = cursor[-4 : -1]
-                        elif 'kernel_size:' in cursor:
-                            layer_data['kernel_size'] = cursor[-2]
-                        elif 'stride:' in cursor:
-                            layer_data['stride'] = cursor[-2]
-                        j = j + 1
+                        if default_stride: layer_data['stride'] = 1
 
-                    if layer_data['type'] == 'Convolution':
-                        if prev_layer_top == '': prev_layer_top = 'input'   # I don't know why the first time it's empty
-                        outfile.write(layers_strings['Convolution'].format(layer_data['name'],
-                                                                           layer_data['name'],
-                                                                           layer_data['num_output'],
-                                                                           layer_data['kernel_size'],
-                                                                           layer_data['stride'],
-                                                                           prev_layer_top))
-                    elif layer_data['type'] == 'Pooling':
-                        outfile.write(layers_strings['Pooling'].format(layer_data['name'],
-                                                                       pool_type[layer_data['pool_type']],
-                                                                       layer_data['kernel_size'],
-                                                                       layer_data['stride'],
-                                                                       prev_layer_top))
-
-                if layer_data['type'] == 'ReLU':
-                    outfile.write(layers_strings['ReLU'].format(prev_layer_name, prev_layer_name))
-                elif layer_data['type'] == 'PReLU':
-                    outfile.write(layers_strings['PReLU'].format(prev_layer_name, layer_data['name'], prev_layer_name))
-                elif layer_data['type'] == 'Concat':
-                    bottoms_str = ''
-                    for bottom in layer_data['bottoms']:
-                        bottoms_str = bottoms_str + bottom + ', '
-                    outfile.write(layers_strings['Concat'].format(layer_data['name'], layer_data['name'], bottoms_str[ : -2]))
-                    layer_data['bottoms'] = []
-
-                layer_data['type'] = ''
-                prev_layer_name = layer_data['name']
-                prev_layer_top = layer_data['top']
-
+            # After the for cycle finished...
             outfile.write(f"keras_model = tf.keras.Model(inputs=input, outputs={layer_data['name']})")
 
 
