efe4148a65999fe50b5d3c9d5915df50d588a56f
AlexPasqua <alexpasquali98@gmail.com>
2020-08-09 14:39:18 +0200

    Loaded k2c_1.py and k2c_2.py to convert keras model to caffe

---
+++ keras2caffe/k2c_1.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Conv2D, ReLU, MaxPooling2D, PReLU, Concatenate
+
+import caffe
+from caffe import layers as cl, params as cp
+
+import numpy as np
+import pickle
+import argparse
+
+
+def save_keras_weights(model_path, weights_path):
+    keras_model = tf.keras.models.load_model(model_path)
+
+    # Since both Keras and Caffe models don't fit in the memory, I create a dictionary with the weights to be copied
+    weights = {}
+    for i in range(len(keras_model.layers)):
+        layer = keras_model.layers[i]
+
+        # Skip pad layers and layers with no weights
+        if layer.name[-3 :] == 'pad' or layer.get_weights() == []:
+            continue
+
+        key = (layer.name, layer.__class__.__name__)
+        weights[key] = layer.get_weights()
+
+    # Save the weights dictionary
+    weights_path = weights_path + ('' if weights_path[-4 : ] == '.pkl' else '.pkl')
+    with open(weights_path, 'wb') as f:
+        pickle.dump(weights, f, pickle.HIGHEST_PROTOCOL)
+
+
+if __name__ == '__main__':
+    pareser= argparse.ArgumentParser(
+        description="Reads the Keras network's parameters and create a picke to export"
+    )
+    parser.add_argument('keras_model', action='store', help="The filename (full path including extension) of the file that contains the Keras model.")
+    parser.add_argument('weights_path', action='store', help="The filename (full path WITHOUT extension) of the file that will contain the Keras model's weights.")
+    args = parser.parse_args()
+    save_keras_weights(args.keras_model, args.weights_path)
---
+++ keras2caffe/k2c_2.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Input, Conv2D, ReLU, MaxPooling2D, PReLU, Concatenate
+
+import caffe
+from caffe import layers as cl, params as cp
+
+import numpy as np
+import pickle
+
+
+def create_caffe_net(prototxt_path, weights_path, caffemodel_path):
+    # Load weights in dictionary form
+    with open(weights_path, 'rb') as f:
+        weights = pickle.load(f)
+
+    # Caffe net
+    caffe.set_device(0)
+    caffe.set_mode_gpu()
+    caffe_model = caffe.Net(prototxt_path, caffe.TEST)
+
+    # Load weights in the Caffe model
+    for k in weights:
+        name = k[0]
+        type = k[1]
+
+        # Skip if there's no layer in the caffe model with the same name
+        if not np.any([x == name for x in caffe_model._layer_names]): continue
+
+        # Convert conv layers
+        if type == 'Conv2D':
+            data = weights[k]
+            w,b = data if len(data) > 1 else [data[0], np.zeros(np.shape(data)[-1])]  # the bias might not be present
+            caffe_model.params[name][0].data[...] = np.transpose(w, (3,2,0,1))  # Caffe wants (c_out, c_in, h, w)
+            caffe_model.params[name][1].data[...] = b
+
+        # Convert PReLU layers
+        elif type == 'PReLU':
+            caffe_model.params[name][0].data[...] = weights[k][0][...][0][0]
+
+        # Convert batchnorm layers
+        elif type == 'BatchNormalization':
+            gamma, beta, mean, variance = weights[k]
+            caffe_model.params[name][0].data[...] = mean
+            caffe_model.params[name][1].data[...] = variance + 1e-3
+            caffe_model.params[name][2].data[...] = 1  # always set scale factor to 1
+            caffe_model.params['{}_sc'.format(name)][0].data[...] = gamma  # scale
+            caffe_model.params['{}_sc'.format(name)][1].data[...] = beta  # bias
+
+    # Save the net into caffemodel
+    caffemodel_path = caffemodel_path + ('' if caffemodel_path[-11 : ] == '.caffemodel' else '.caffemodel')
+    caffe_model.save(caffemodel_path)
+
+
+if __name__ == '__main__':
+    pareser= argparse.ArgumentParser(
+        description="Reads the Keras model's parameters from a previously exported pickle, load them into a Caffe net and saves it"
+    )
+    parser.add_argument('prototxt', action='store', help="The filename (full path including file extension) of the '.prototxt' file that defines the Caffe model.")
+    parser.add_argument('weights_path', action='store', help="The filename (full path WITHOUT extension) of the file that will contain the Keras model's weights.")
+    parser.add_argument('caffemodel', action='store', help="The filename (full path WITHOUT extension) of the file where to save the Caffe model")
+    args = parser.parse_args()
+    save_keras_weights(args.keras_model, args.weights_path, args.caffemodel)
